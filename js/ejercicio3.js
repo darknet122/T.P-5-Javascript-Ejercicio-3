e6ec17382b2a478406041695876ea40d817808a8
Tomas Quispe <tomasbquispe@gmail.com>
2023-04-15 17:00:29 -0300

    agregue info al ejercicio 3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,10 +1,11 @@
-// Info date
+//3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin. 
+
+
 const dateNumber = document.getElementById('dateNumber');
 const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
 
-// Tasks Container
 const tasksContainer = document.getElementById('tasksContainer');
 
 const setDate = () => {
